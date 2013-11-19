1aa47f6f570fd3b4324c5c70861d4744d2b40546
Xiong Shu <xiongshu@guest-wireless-upc-nat-206-117-88-005.usc.edu>
2013-11-19 14:58:50 -0800

    Initial Commit

---
+++ cvhw5/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cvhw5
+//
+//  Created by Xiong Shu on 11/19/13.
+//  Copyright (c) 2013 Xiong Shu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
